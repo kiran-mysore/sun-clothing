b83b46dd363515ba92edcadd193a4b3b03dcf703
Kiran Rao <kiran.mysore.rao@gmail.com>
2020-08-30 15:06:40 +0200

    Added the withRouter HOC to the menu-items component for navigation

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './homepage.styles.scss';
-import MenuItem from '../components/menu-item/menu-item.component';
 import Directory from '../directory/directory.component';
 
 const HomePage = () => {
